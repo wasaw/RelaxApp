5f64127e932933d6644fd676d77e9eebf0d7d0ef
wasaw <merenkovalexander@gmail.com>
2022-12-15 15:13:51 +0300

    Инициализация репозитория

---
+++ RelaxApp/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  RelaxApp
+//
+//  Created by Александр Меренков on 15.12.2022.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ RelaxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RelaxApp
+//
+//  Created by Александр Меренков on 15.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
