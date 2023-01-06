fd4596d43191d624e31346b1153c48c1a1c77459
wasaw <merenkovalexander@gmail.com>
2023-01-06 22:43:54 +0300

    Реализована tableView

---
+++ RelaxApp/Completed/CompletedCell.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedCell.swift
+//  RelaxApp
+//
+//  Created by Александр Меренков on 06.01.2023.
+//
+
+import UIKit
+
+final class CompletedCell: UITableViewCell {
+    static let identifire = "tableCell"
+    
+//    MARK: - Properties
+    
+//    MARK: - Lifecycle
+    
+    
+}
--- RelaxApp/Completed/CompletedViewController.swift
+++ RelaxApp/Completed/CompletedViewController.swift
@@ -18,12 +18,29 @@ final class CompletedViewController: UIViewController {
     var presenter: CompletedPresenterProtocol?
     let configurator: CompletedConfiguratorProtocol = CompletedConfigurator()
     
+    var tableView: UITableView?
+    
 //    MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configurator.config(with: self)
+        configUI()
+    }
+    
+//    MARK: - Helpers
+    
+    private func configUI() {
+        tableView = UITableView(frame: .zero)
+        guard let tableView = tableView else { return }
+        tableView.register(CompletedCell.self, forCellReuseIdentifier: CompletedCell.identifire)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+        
+        tableView.anchor(left: view.leftAnchor, top: view.safeAreaLayoutGuide.topAnchor, right: view.rightAnchor, bottom: view.bottomAnchor)
+        tableView.backgroundColor = .background
     }
     
 }
@@ -32,4 +49,24 @@ final class CompletedViewController: UIViewController {
 
 extension CompletedViewController: CompletedViewProtocol {
     
+}
+
+//  MARK: - UITableViewDelegate
+
+extension CompletedViewController: UITableViewDelegate {
+    
+}
+
+//  MARK: - UITableViewDataSource
+
+extension CompletedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CompletedCell.identifire) as? CompletedCell else { return UITableViewCell() }
+        
+        return cell
+    }
 }
